23e97a0d8e0c779ac2023babbeabf2478f11a251
98Suzanne <s.dinten@upcmail.nl>
2022-04-05 12:17:56 +0200

    Moved weather app to VS Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Contact from "./Contact";
+import Search from "./Search";
+import Forecast from "./Forecast";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <div className="container">
+          <Search />
+          <Forecast />
+          <hr />
+          <div className="weather-forecast" id="forecast"></div>
+        </div>
+        <Contact />
+      </body>
     </div>
   );
 }
-
-export default App;
---
+++ src/Contact.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div>
+      <a
+        href="https://github.com/98Suzanne/Weather-app"
+        className="github"
+        target="blank"
+      >
+        Open source code
+      </a>
+      <div id="contact">
+        <span>by </span>
+        <a
+          href="https://www.linkedin.com/in/svandinten/"
+          className="linkedin"
+          target="blank"
+        >
+          Suzanne van Dinten
+        </a>
+      </div>
+    </div>
+  );
+}
